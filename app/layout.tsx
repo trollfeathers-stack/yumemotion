ecd9216f93d1c92f5b7509251f1e4bb841144156
YumeMotion <284357547+yumemotionai@users.noreply.github.com>
2026-05-14 21:08:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-{/* analytics deployment update */}
+{/* analytics final deployment update */}
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
